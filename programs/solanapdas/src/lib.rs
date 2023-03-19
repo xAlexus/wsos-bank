a813cf8426eda0f372f41424c1934b6577a29b3f
Alex <akorolonek@yahoo.com>
2023-03-19 19:22:36 +0100

    Upgraded for Lesson 5

--- programs/solanapdas/src/lib.rs
+++ programs/solanapdas/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("rS4J7km6kaKJcUnNSR6TTocRrEqm97yQMdEwG5AW1Tv");
 
 #[program]
 pub mod solanapdas {
@@ -44,6 +44,7 @@ pub mod solanapdas {
         }
         **bank.to_account_info().try_borrow_mut_lamports()? -= amount;
         **user.to_account_info().try_borrow_mut_lamports()? += amount;
+        (&mut ctx.accounts.bank).balance -= amount;
         Ok(())
     }
 }
